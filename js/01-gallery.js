f0673236943b77bd0cc8c6211bcfc158584edabc
jullyrud <94559083+jullyrud@users.noreply.github.com>
2022-11-16 17:54:01 +0100

    hw 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,45 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryRefs = document.querySelector('.gallery')
+
+galleryRefs.innerHTML = makeNewRender(galleryItems)
+
+galleryRefs.addEventListener('click', openModal)
+
+function makeNewRender(gallery) {
+    return gallery.map(i => 
+ `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${i.preview}"
+      data-source="${i.original}"
+      alt="${i.description}"
+    />
+  </a>
+</div>`).join('')
+}
+makeNewRender(galleryItems)
+
+function openModal(e) {
+    e.preventDefault()
+    console.log(e.target.dataset.source);
+    const instance = basicLightbox.create(`<img src="${e.target.dataset.source}" alt="" ></img>`, {
+      onShow: (instance) => {
+        document.addEventListener("keydown", eventOnModal)
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", eventOnModal)
+      }
+    })
+  
+  instance.show()
+  function eventOnModal(e) {
+    if (e.code === 'Escape') {
+             instance.close()
+           }
+} 
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,24 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryRefs = document.querySelector('.gallery')
+
+galleryRefs.innerHTML = makeNewRender(galleryItems)
+
+function makeNewRender(galery) {
+    return galery.map(i =>  
+    `<a class="gallery__item" 
+    href="${i.original}">
+    <img class="gallery__image" 
+    src="${i.preview}" 
+    alt="${i.description}" />
+    </a>`).join('')
+}
+
+galleryRefs.addEventListener('click', openModal())
+
+function openModal() {
+    var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+}
